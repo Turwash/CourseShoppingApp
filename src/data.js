7fa6e252f82b34078148438788a5b78966c2b1c2
Turwash Chakraborty <121122397+Turwash@users.noreply.github.com>
2023-04-18 21:40:57 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "React Js Training",
+    img: "img/product-1.png",
+    price: 100,
+    company: "Turwash",
+    info:
+    "React JS is an open-source JavaScript library that is used for building user interfaces for web and mobile applications. It was developed by Facebook and was first released in 2013. React is known for its ability to create reusable UI components and for its virtual DOM (Document Object Model) which allows for efficient rendering of changes to the UI.React works by breaking down a user interface into smaller, reusable components, which can then be combined to form complex interfaces.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Javascript Course",
+    img: "img/product-2.png",
+    price: 160,
+    company:  "Turwash",
+    info:
+      "JavaScript is a high-level, interpreted programming language that is primarily used to create interactive and dynamic web content. It is a versatile language that can run on both the client-side and server-side, making it a crucial component of modern web development. JavaScript is supported by all modern web browsers and is used extensively to create interactive web pages, web applications, and mobile applications. It can also be used to develop desktop applications and games. JavaScript is known for its flexibility and ease of use, and its popularity has led to the development of numerous libraries and frameworks that simplify the process of web development.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "DSA - Self Placed",
+    img: "img/product-3.png",
+    price: 800,
+    company: "Turwash",
+    info:
+      "DSA stands for Data Structures and Algorithms. It is a fundamental concept in computer science and software engineering that deals with the organization and manipulation of data and the design and analysis of algorithms that operate on that data. Data structures refer to the way data is organized and stored in a computer's memory, while algorithms refer to the step-by-step procedures used to solve problems or perform operations on that data. Data structures and algorithms are essential to the development of efficient and scalable software applications. They are used to solve a wide range of computing problems, from sorting and searching large data sets to optimizing complex systems. Understanding data structures and algorithms is important for all software developers, as it can help them write more efficient and reliable code.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Full stack WebDevelopment",
+    img: "img/product-4.png",
+    price: 180,
+    company: "Turwash",
+    info:
+      "Full stack web development refers to the development of both the front-end (client-side) and back-end (server-side) portions of a web application. A full stack web developer has the skills and knowledge to work on all aspects of a web application, including the user interface, server-side logic, database integration, and deployment. Full stack web development requires a deep understanding of various technologies and programming languages, as well as the ability to work with multiple systems and platforms. It is a challenging but rewarding field that allows developers to create complex and robust web applications from start to finish.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "DevOps Training",
+    img: "img/product-5.png",
+    price: 120,
+    company:  "Turwash",
+    info:
+      "DevOps is a set of practices that combines software development (Dev) and IT operations (Ops) to shorten the development life cycle and increase the reliability and efficiency of software delivery. It is a cultural shift that emphasizes collaboration and communication between development teams and operations teams, with the goal of automating processes and streamlining workflows.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Bootstrap Training",
+    img: "img/product-6.png",
+    price: 100,
+    company:  "Turwash",
+    info:
+      "Bootstrap is a free, open-source front-end framework for building responsive, mobile-first websites and web applications. It was originally developed by Twitter and is now maintained by a community of developers. Bootstrap provides a set of pre-built HTML, CSS, and JavaScript components that can be used to quickly and easily create responsive web pages and user interfaces. These components include things like navigation bars, forms, buttons, typography, and more.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "HTML Full Course",
+    img: "img/product-7.png",
+    price: 80,
+    company:  "Turwash",
+    info:
+      "HTML (Hypertext Markup Language) is a standard markup language used for creating and structuring content on the web. It is a fundamental technology used to build web pages and web applications. HTML uses a series of tags to structure and format text, images, and other content, allowing web browsers to display content in a consistent and organized way. HTML tags are used to create headings, paragraphs, lists, links, and more, and can be combined with CSS (Cascading Style Sheets) to control the presentation and layout of web pages. HTML documents are typically saved with a .html or .htm file extension, and can be created using a simple text editor or specialized web development software.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Mern Stack",
+    img: "img/product-8.png",
+    price: 2000,
+    company:  "Turwash",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
